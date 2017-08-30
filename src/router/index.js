8fa91524011fa06c99223b681a0062ed79ef1cdb
GarenLiang <ideabreeze@gmail.com>
2017-08-30 23:27:35 +0800

    goods list view

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GoodsList from './../views/GoodsList';
-import Title from '@/views/Title';
-import Image from '@/views/Image';
-import Cart from '@/views/Cart';
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/goods',
-      name: 'GoodsList',
-      components: {
-        default:GoodsList,
-        title: Title,
-        img: Image
-      }
-    },
-    {
-      path: '/cart/:cartId',
-      name: 'cart',
-      component: Cart
+      path: '/',
+      component: GoodsList
     }
   ]
 })
